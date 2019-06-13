2ff2a6b675ee57c574918b192b1d34e22ba78cf7
Michael Rosario <mjfrosario@gmail.com>
2019-06-12 22:08:34 -0400

    work on react search

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,19 +2,8 @@ import axios from "axios";
 
 export default {
   // Gets all books
-  getBooks: function() {
-    return axios.get("/api/books");
+  checkSite: function(site) {
+    return axios.get("/api/check/"+site);
   },
-  // Gets the book with the given id
-  getBook: function(id) {
-    return axios.get("/api/books/" + id);
-  },
-  // Deletes the book with the given id
-  deleteBook: function(id) {
-    return axios.delete("/api/books/" + id);
-  },
-  // Saves a book to the database
-  saveBook: function(bookData) {
-    return axios.post("/api/books", bookData);
-  }
+
 };
