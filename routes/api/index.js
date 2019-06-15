fe7da564f1c2180334133a67c44b6dab8e79414a
geeyan.bootcamp <geeyan.bootcamp@gmail.com>
2019-06-15 12:06:57 -0400

    start_cheerio

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
-//const bookRoutes = require("./books");
+const axios = require("axios");
+const cheerio = require("cheerio");
 
 // Book routes
 //router.use("/books", bookRoutes);
@@ -8,7 +9,24 @@ router.get("/check/:site", function(req,res){
       let site = req.params.site;
       // remove http amd https and use www   
       console.log(site);
-      return res.json(site);
+      axios.get("https://"+site)
+        .then(response => {
+          //console.log(response.data);
+          const $  = cheerio.load(response.data);
+
+          const output = [];
+
+          $("meta").each((i, element) => {
+              output.push({
+                name: $(element).attr("name"),
+                content: $(element).attr("content")
+              });
+          });
+
+          console.log("output",output);
+          return res.json(output);
+        }).catch( error => console.log(error));
+
   })
   
 
