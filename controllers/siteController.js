6e06456a3dca4d6654c928d444827c02a95fc64a
Geeyan <geeyan.bootcamp@gmail.com>
2019-06-19 19:05:07 -0400

    updated schema and created crud

---
+++ controllers/siteController.js
@@ -0,0 +1,39 @@
+const db = require("../models");
+
+// Defining methods for the booksController
+module.exports = {
+  findAll: function(req, res) {
+    db.Sites
+      .find(req.query)
+      .populate('site_badges')
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.Sites
+      .findById(req.params.id)
+      .populate('site_badges')
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.Sites
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+    db.Sites
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.Sites
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
