323d2fc8003018eb8a931635c28679e1f8bd4217
Han Seoulo <cu.rogue.coder@gmail.com>
2019-06-19 22:26:17 -0400

    implimented cards, navigation drop down, navigation search bar using react-bootstrap npm package

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,12 +1,28 @@
 import React from "react";
+import { NavDropdown, Navbar,Form, Button,FormControl } from 'react-bootstrap';
+
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">
-        Super Sexy Site Checker
-      </a>
-    </nav>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      
+      
+      <Navbar.Brand href="#home">Super Sexy Site Checker</Navbar.Brand>
+
+      <Form inline>
+        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+        <Button variant="dark">Search</Button>
+      </Form>
+      <NavDropdown className="nav-dark" title="Dropdown" id="collasible-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+    
+      
+    </Navbar>
   );
 }
 
