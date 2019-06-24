bb538b7c79c300c04276ee16d2cd1588225659d6
Han Seoulo <cu.rogue.coder@gmail.com>
2019-06-24 00:08:31 -0400

    adjusting front end

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -5,25 +5,7 @@ import { NavDropdown, Navbar,Form, Button,FormControl } from 'react-bootstrap';
 function Nav() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      
-      
-      <Navbar.Brand href="#home">Super Sexy Site Checker</Navbar.Brand>
-
-      <Form inline>
-        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-        <Button variant="dark">Search</Button>
-      </Form>
-      <NavDropdown className="nav-dark" title="Dropdown" id="collasible-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-      </NavDropdown>
-    
-      
+      <Navbar.Brand href="#home">CODE HYPE</Navbar.Brand>
     </Navbar>
   );
 }
---
+++ client/src/pages/SignIn.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+//import DeleteBtn from "../components/DeleteBtn";
+import Jumbotron from "../components/Jumbotron";
+import API from "../utils/API";
+//import { Link } from "react-router-dom";
+import { Col, Row, Container } from "../components/Grid";
+import { Input, FormBtn } from "../components/Form";
+import socket from 'socket.io-client';
+import {Card} from 'react-bootstrap';
+
