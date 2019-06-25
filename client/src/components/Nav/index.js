973e607d1c323019ceb28ab790808bb4a663bec7
Han Seoulo <cu.rogue.coder@gmail.com>
2019-06-24 21:08:06 -0400

    added sign-in modal for Franchesca

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import SignInModal from "../SignIn"
 import {Navbar} from 'react-bootstrap';
 
 
@@ -6,6 +7,7 @@ function Nav() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Navbar.Brand href="#home"><img src="/assets/images/logo/logo-coderhype.svg" alt="coderhype"/></Navbar.Brand>
+      <SignInModal />
     </Navbar>
   );
 }
