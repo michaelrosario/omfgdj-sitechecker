34d567ea1c0ef1f4e1f6b7a34e17abeaf7120113
Michael Rosario <mjfrosario@gmail.com>
2019-06-25 18:32:38 -0400

    add pages and test react-router

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,13 +1,23 @@
 import React from "react";
 import SignInModal from "../SignIn"
-import {Navbar} from 'react-bootstrap';
+import { Navbar } from 'react-bootstrap';
+import { NavLink } from "react-router-dom";
+import 'font-awesome/css/font-awesome.min.css';
 
 
 function Nav() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Navbar.Brand href="#home"><img src="/assets/images/logo/logo-coderhype.svg" alt="coderhype"/></Navbar.Brand>
+      <NavLink to="/" exact activeStyle={{ color: 'red' }}>
+        <Navbar.Brand href="#home"><img src="/assets/images/logo/logo-coderhype.svg" alt="coderhype"/></Navbar.Brand>
+      </NavLink>
       <SignInModal />
+      <NavLink to="/account" exact activeStyle={{ color: 'red' }}>
+              <i className="fa fa-user"></i> &nbsp; My Account
+      </NavLink>
+      <NavLink to="/user" exact activeStyle={{ color: 'red' }}>
+              <i className="fa fa-link"></i> &nbsp; Sites
+      </NavLink>
     </Navbar>
   );
 }
---
+++ client/src/pages/Account.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import { Col, Row, Container } from "../components/Grid";
+
+
+class Account extends Component {
+  state = {
+  
+  };
+ 
+
+  componentDidMount() {
+  
+    
+  }
+
+ 
+
+
+  render() {
+
+   
+    return (
+      <Container fluid>
+        <Row>
+          <Col size="md-4">
+            <h1>Your Account</h1>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default Account;
