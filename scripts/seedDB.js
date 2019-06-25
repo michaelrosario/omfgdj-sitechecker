16f0d182a09d3c29fa93e6669c0fad77b2aee1ae
Geeyan <geeyan.bootcamp@gmail.com>
2019-06-25 12:34:42 -0400

    updated seedDB.js-user and site seeds

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -10,132 +10,62 @@ mongoose.connect(
 
 const badgeSeed = [
   {
-
-      badge_name: "HTML Not Broken",
-      badge_description: "Good",
-      badge_score: 5,
-      badge_icon: "/image",
-},
-  {
-      badge_name: "HTML Broken",
-      badge_description: "Broken",
-      badge_score: -5,
-      badge_icon: "/image",
+    badge_name: "React",
+    badge_version: "16.3.0",
+    badge_icon: "React.png",
+    badge_category: "Javascript Frameworks", // Omar: view RES object. need to properly get this data from res
+    badge_score: 1,
   },
   {
-      badge_name: "HTML 5",
-      badge_description: "HTML 5",
-      badge_score: 5,
-      badge_icon: "/image",
-},
-{
-      badge_name: "HTML 4",
-      badge_description: "HTML 4",
-      badge_score: 4,
-      badge_icon: "/image",
-},
-{
-      badge_name: "jQuery",
-      badge_description: "jQuery framework",
-      badge_score: 5,
-      badge_icon: "/image",
-},
-{
-      badge_name: "React",
-      badge_description: "React framework",
-      badge_score: 5,
-      badge_icon: "/image",
-},
-{
-      badge_name: "Angular",
-      badge_description: "Angular framework",
-      badge_score: 5,
-      badge_icon: "/image",
-},
-{
-      badge_name: "Bootstrap",
-      badge_description: "Bootstrap Design Framework",
-      badge_score: 5,
-      badge_icon: "/image",
-},
-{
-      badge_name: "Foudation",
-      badge_description: "Foudation Design Framework",
-      badge_score: 5,
-      badge_icon: "/image",
-},
-{
-      badge_name: "No Design Framework",
-      badge_description: "No Design Framework",
-      badge_score: -5,
-      badge_icon: "/image",
-},
-{
-      badge_name: "Google",
-      badge_description: "Google Font",
-      badge_score: 5,
-      badge_icon: "/image",
-},
-{
-      badge_name: "Adobe",
-      badge_description: "Adobe TypeKit Font",
-      badge_score: 10,
-      badge_icon: "/image",
-},
-{
-      badge_name: "Default font",
-      badge_description: "Default font",
-      badge_score: 5,
-      badge_icon: "/image",
-},
-{
-      badge_name: "Facebook",
-      badge_description: "Facebook Social Media",
-      badge_score: "5",
-      badge_icon: "/image",
+    badge_name: "Nginx",
+    badge_version: "", // Omar: RES object is NULL, this field is not required in Schema so i am leaving the dummy data blank 
+    badge_icon: "Nginx.svg",
+    badge_category: "Web Servers",
+    badge_score: 1,
+  },
+  {
+    badge_name: "jQuery",
+    badge_version: "1.8.3",
+    badge_icon: "jQuery.svg",
+    badge_category: "JavaScript Libraries",
+    badge_score: 1,
+  },
+  {
+    badge_name: "Google Analytics",
+    badge_version: "", // Omar: another NULL response, we dont really need this so I guess we can remote this in the Schema and here???
+    badge_icon: "Google Analytics.svg",
+    badge_category: "Analytics",
+    badge_score: 1,
   },
   {
     badge_name: "Twitter",
-    badge_description: "Twitter Media",
-    badge_score: "5",
-    badge_icon: "/image",
-},
-{
-  badge_name: "Instagram",
-  badge_description: "Instagram Media",
-  badge_score: "5",
-  badge_icon: "/image",
-},
-{
-  badge_name: "Blogger",
-  badge_description: "Blogger",
-  badge_score: "5",
-  badge_icon: "/image",
-},
-{
-  badge_name: "No blogger",
-  badge_description: "No Blogger used",
-  badge_score: "-5",
-  badge_icon: "/image",
-},
-{
-  badge_name: "Http Request Greater than 5",
-  badge_description: "Http Request Greater than 5",
-  badge_score: "5",
-  badge_icon: "/image",
-},
-{
-  badge_name: "Http Request less than 5",
-  badge_description: "Http Request less than 5",
-  badge_score: "3",
-  badge_icon: "/image",
-},
-{
-  badge_name: "Static",
-  badge_description: "No http request",
-  badge_score: "-5",
-  badge_icon: "/image",
-}];
+    badge_version: "",
+    badge_icon: "Twitter.svg",
+    badge_category: "Widgets",
+    badge_score: 1,
+  },
+  {
+    badge_name: "Bootstrap",
+    badge_version: "",
+    badge_icon: "Bootstrap.png",
+    badge_category: "Web Frameworks",
+    badge_score: 1,
+  },
+  {
+    badge_name: "AngularJS",
+    badge_version: "",
+    badge_icon: "AngularJS.svg",
+    badge_category: "JavaScript Frameworks",
+    badge_score: 1,
+  },
+  {
+    badge_name: "Apache",
+    badge_version: "",
+    badge_icon: "Apache.svg",
+    badge_category: "Web Servers",
+    badge_score: 1,
+  }
+];
 
 db.Badges
   .remove({})
@@ -152,11 +82,47 @@ db.Badges
 
 const userSeed = [
     {
-      user_login: "user",
-      user_password: "password",
-      user_email: "email",
-      user_firstname: "FirstName",
-      user_lastname: "LastName",
+      user_login: "james",
+      user_password: "james123",
+      user_email: "tagorda@gmail.com",
+      user_firstname: "James",
+      user_lastname: "Tagorda",
+      user_phone: "123-456-7891",
+      user_imgsrc: "",
+      user_github: "http://github.com",
+      user_preference: "undefined",
+      user_sites: []
+    },
+    {
+      user_login: "Francesca",
+      user_password: "Francesca123",
+      user_email: "francesca@gmail.com",
+      user_firstname: "Francesca",
+      user_lastname: "Gonzalez",
+      user_phone: "123-456-7891",
+      user_imgsrc: "",
+      user_github: "http://github.com",
+      user_preference: "undefined",
+      user_sites: []
+    },
+    {
+      user_login: "Omar",
+      user_password: "omar123",
+      user_email: "omar@gmail.com",
+      user_firstname: "Omar",
+      user_lastname: "Ong",
+      user_phone: "123-456-7891",
+      user_imgsrc: "",
+      user_github: "http://github.com",
+      user_preference: "undefined",
+      user_sites: []
+    },
+    {
+      user_login: "Danny",
+      user_password: "danny123",
+      user_email: "danny@gmail.com",
+      user_firstname: "Danny",
+      user_lastname: "Lee",
       user_phone: "123-456-7891",
       user_imgsrc: "",
       user_github: "http://github.com",
@@ -164,6 +130,7 @@ const userSeed = [
       user_sites: []
     }
 ];
+
 
 db.User
 .remove({})
@@ -179,6 +146,27 @@ db.User
 
 
   const sitesSeed = [
+    {
+      site_name: "Yelp",
+      site_url: "www.yelp.com",
+      site_desc: "yelp site",
+      site_imgsrc: "",
+      site_badges: [] // how to populate with assigned badges
+    },
+    {
+      site_name: "MLB",
+      site_url: "www.mlb.com",
+      site_desc: "Major League Baseball",
+      site_imgsrc: "",
+      site_badges: [] // how to populate with assigned badges
+    },
+    {
+      site_name: "Homedepot",
+      site_url: "www.homedepot.com",
+      site_desc: "Homedepot Website",
+      site_imgsrc: "",
+      site_badges: [] // how to populate with assigned badges
+    },
     {
       site_name: "Columbia University",
       site_url: "www.columbia.edu",
