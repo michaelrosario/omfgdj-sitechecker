33ee2fd9527c45c72cb39ff4e521d4439527a7bc
Geeyan <geeyan.bootcamp@gmail.com>
2019-06-27 15:45:11 -0400

    added alerts to navigation tab

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -2,13 +2,17 @@ import React, { Component } from "react";
 import SignInModal from "../SignIn"
 import { Navbar } from 'react-bootstrap';
 import { NavLink } from "react-router-dom";
+import Alert from "../Alert";
+import socket from 'socket.io-client';
 import 'font-awesome/css/font-awesome.min.css';
 import "./style.css";
 
 class Nav extends Component {
 
   state = {
-    isLoggedIn: false
+    alert:"",
+    isLoggedIn: false,
+    endpoint: process.env.NODE_ENV === "production" ? "/" : "localhost:3001"
   }
 
   handleLoggedIn = bool => {
@@ -17,9 +21,36 @@ class Nav extends Component {
     })
   }
 
+  showAlert = message => {
+    //console.log("message",message);
+    this.setState({ 
+      alert: message, 
+    });
+    setInterval(() => {
+      this.setState({ 
+        alert: "", 
+      });
+    }, 5000);
+  }
+
+  componentDidMount() {
+    const io = socket(this.state.endpoint, { secure: true });
+    
+    io.on('toReact',site => {
+      console.log(site);
+      this.showAlert(site.data.data)
+    })
+  }
+
+  componentWillUnmount() {
+    const io = socket(this.state.endpoint, { secure: true });
+    io.off("toReact");
+
+  }
   render() {
 
     return (
+      <div>
       <Navbar className="main-nav" collapseOnSelect expand="lg" bg="dark" variant="dark">
         <NavLink to="/" exact activeStyle={{ color: 'red' }} className="logo">
           <img src="/assets/images/logo/logo-coderhype.svg" alt="coderhype"/>
@@ -51,6 +82,10 @@ class Nav extends Component {
       
         <div className={this.state.isLoggedIn ? "modal-links logged-in" : "modal-links logged-out"}><SignInModal setUserSession={this.handleLoggedIn} /></div>
       </Navbar>
+    
+      <Alert message={this.state.alert} />
+          
+      </div>
 
     );
   }
