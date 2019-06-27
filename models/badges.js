619e3c1e9b92b0756ce97bfd03989c5afd7a0df2
Geeyan <geeyan.bootcamp@gmail.com>
2019-06-27 17:48:55 -0400

    badge_component has been added to badges.js

--- models/badges.js
+++ models/badges.js
@@ -5,6 +5,7 @@ const badgeSchema = new Schema({
   badge_name: { type: String, required: true }, // Wappa: res.name
   badge_version: { type: String, required: false }, // W: res.version IMPT: some results are null so NOT REQUIRED
   badge_icon: { type: String, required: true}, // W: res.icon
+  badge_component: { type: String, required: true},
   badge_category: { type: String, required: true}, // W: res.categories[0][0]
   badge_score: { type: Number, required: true }, // Our value (1)
 });
