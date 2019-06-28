4c1a547d9055798d386238a202f602be96c3f6c0
Omar <marionong@gmail.com>
2019-06-28 01:57:10 -0400

    badge components modeled(copied) from AngularJS

--- client/src/badges/Apache/index.js
+++ client/src/badges/Apache/index.js
@@ -1,18 +1,36 @@
 import React from "react";
 import 'font-awesome/css/font-awesome.min.css';
 
-const Apache(props) => {
-  let siteData = props.siteData;
+const Apache = (props) => {
+
+  let siteData = props.siteData || {};
+  let showStyle = {
+    display: "none"
+  }
+  //console.log(siteData);
+  
+  if(Object.keys(siteData).length !== 0 && siteData.constructor === Object && siteData.wappalyzer.length) {
+    if(siteData.wappalyzer.some(item => item.name === props.badge.badge_name)){
+      console.log(props.badge.badge_name + " FOUND");
+      // run only once
+      props.updateScore(props.badge.badge_score,props.badge._id);
+      showStyle = {
+        display: "block"
+      }
+    }
+  } else {
+    //console.log(props.badge.badge_name + " NOT FOUND");
+  }
+
 
 return (
-    <div>
-      Hello Apache! 
-      {siteData.title}
-      {props.badge.badge_name}<br />
-      {props.badge._id}<br />
-      {props.badge.badge_image}<br />
-      {props.badge.badge_score}<br />
-      <button onClick={() => props.updateScore(10)}>CLICK</button>
+    <div style={showStyle}>
+      <img 
+        src={"https://www.wappalyzer.com/images/icons/"+props.badge.badge_icon} 
+        alt={props.badge.badge_name} 
+        width={100} height={100} />
+      <h3>{props.badge.badge_name}</h3>
+      
     </div>
   );
 }
