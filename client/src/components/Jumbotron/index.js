a15924da037e3b61e1b45068ffc492ca6118d433
Han Seoulo <cu.rogue.coder@gmail.com>
2019-06-28 02:02:19 -0400

    modified aboutus page - added jumbotron - added placeholder images

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -10,8 +10,9 @@ function Jumbotron({ children }) {
         clear: "both", 
         paddingTop: 120, 
         textAlign: "center", 
+        backgroundImage: "url(https://i.pinimg.com/originals/1d/15/ef/1d15ef35ba5f1928a0e81065a503c17e.jpg", backgroundRepeat: 'repeat',
         backgroundPosition: 'center', 
-        backgroundSize: 'cover'}}
+        backgroundSize: '25%'}}
       className="jumbotron"
     >
       {children}
