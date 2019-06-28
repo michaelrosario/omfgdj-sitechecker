4109333470112bcb21d4a9f2047ada161f7d5452
JamesT <tagorda@gmail.com>
2019-06-27 21:03:00 -0400

    Cards for about us page

--- client/src/pages/Aboutus.js
+++ client/src/pages/Aboutus.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Col, Row, Container } from "../components/Grid";
+import { Card, CardDeck} from "react-bootstrap";
 
 class Aboutus extends Component {
 
@@ -9,13 +10,113 @@ class Aboutus extends Component {
 
 
         return (
-            <Container fluid>
+            <Container fluid center >
                 <Row>
-                    <Col size="md-4">
-                        <h1>About Us</h1>
+                    <Col size="md-12">
+                        <h1 style={{color: '#fff'}}>About Us</h1>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col size="md-12" center>
+                        <CardDeck style={{marginTop: '20px'}}>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+                                <Card.Body>
+                                    <Card.Title>Michael Rosario</Card.Title>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make up the bulk of
+                                        the card's content.
+                                    </Card.Text>
+                                </Card.Body>
+                                <Card.Body>
+                                    <Card.Link href="#">LInkedIn</Card.Link>
+                                    <Card.Link href="#">Github</Card.Link>
+                                </Card.Body>
+                            </Card>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+                                <Card.Body>
+                                    <Card.Title>Michael Rosario</Card.Title>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make up the bulk of
+                                        the card's content.
+                                    </Card.Text>
+                                </Card.Body>
+                                <Card.Body>
+                                    <Card.Link href="#">LInkedIn</Card.Link>
+                                    <Card.Link href="#">Github</Card.Link>
+                                </Card.Body>
+                            </Card>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+                                <Card.Body>
+                                    <Card.Title>Michael Rosario</Card.Title>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make up the bulk of
+                                        the card's content.
+                                    </Card.Text>
+                                </Card.Body>
+                                <Card.Body>
+                                    <Card.Link href="#">LInkedIn</Card.Link>
+                                    <Card.Link href="#">Github</Card.Link>
+                                </Card.Body>
+                            </Card>
+                        </CardDeck>
+                    </Col>
+                </Row>
+                <Row style={{paddingTop: '20px'}}>
+                    <Col size="md-12" center>
+                        <CardDeck style={{marginTop: '20px'}}>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+                                <Card.Body>
+                                    <Card.Title>Michael Rosario</Card.Title>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make up the bulk of
+                                        the card's content.
+                                    </Card.Text>
+                                </Card.Body>
+                                <Card.Body>
+                                    <Card.Link href="#">LInkedIn</Card.Link>
+                                    <Card.Link href="#">Github</Card.Link>
+                                </Card.Body>
+                            </Card>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+                                <Card.Body>
+                                    <Card.Title>Michael Rosario</Card.Title>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make up the bulk of
+                                        the card's content.
+                                    </Card.Text>
+                                </Card.Body>
+                                <Card.Body>
+                                    <Card.Link href="#">LInkedIn</Card.Link>
+                                    <Card.Link href="#">Github</Card.Link>
+                                </Card.Body>
+                            </Card>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+                                <Card.Body>
+                                    <Card.Title>Michael Rosario</Card.Title>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make up the bulk of
+                                        the card's content.
+                                    </Card.Text>
+                                </Card.Body>
+                                <Card.Body>
+                                    <Card.Link href="#">LInkedIn</Card.Link>
+                                    <Card.Link href="#">Github</Card.Link>
+                                </Card.Body>
+                            </Card>
+                        </CardDeck>
                     </Col>
                 </Row>
             </Container>
+
+
+
+
         );
     }
 }
