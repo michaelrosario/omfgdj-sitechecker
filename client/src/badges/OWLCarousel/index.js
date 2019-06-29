53db8a6ad99042c1e37c14ddcd5fdd7920cb81ed
Geeyan <geeyan.bootcamp@gmail.com>
2019-06-29 00:12:29 -0400

    added new wappalyzer badges

---
+++ client/src/badges/OWLCarousel/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Badge from "../../components/Badge";
+
+const OWLCarousel = (props) => {
+
+  let siteData = props.siteData || {};
+  let showStyle = false;
+  //console.log(siteData);
+  
+  if(Object.keys(siteData).length !== 0 && siteData.constructor === Object && siteData.wappalyzer.length) {
+    if(siteData.wappalyzer.some(item => item.name === props.badge.badge_name)){
+      console.log(props.badge.badge_name + " FOUND");
+      
+      // run only once
+      props.updateScore(props.badge.badge_score,props.badge._id);
+      showStyle = true;
+    }
+  } 
+
+  let icon = "https://www.wappalyzer.com/images/icons/"+props.badge.badge_icon;
+
+  return <Badge 
+            show={showStyle} 
+            name={props.badge.badge_name}
+            icon={icon}
+            description={props.badge.badge_description}
+            score={props.badge.badge_score}
+        />;
+}
+
+
+export default OWLCarousel;
