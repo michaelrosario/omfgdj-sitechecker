d74c66ade85b19fc1d8f25f2f4e2c1355c5d6cb6
Michael Rosario <mjfrosario@gmail.com>
2019-06-29 01:45:32 -0400

    fix titan

--- client/src/badges/Titan/index.js
+++ client/src/badges/Titan/index.js
@@ -5,22 +5,26 @@ const Titan = (props) => {
 
   let siteData = props.siteData || {};
   let showStyle = false;
+  //console.log(siteData);
   
-  if(Object.keys(siteData).length !== 0 && siteData.constructor === Object && siteData.meta.length) {
-    if(siteData.meta.some(item => item.name === "viewport")){
+  if(Object.keys(siteData).length !== 0 && siteData.constructor === Object && siteData.wappalyzer.length) {
+    if(siteData.wappalyzer.some(item => item.name === props.badge.badge_name)){
       console.log(props.badge.badge_name + " FOUND");
+      
       // run only once
       props.updateScore(props.badge.badge_score,props.badge._id);
       showStyle = true;
     }
   } 
 
-return <Badge 
-          show={showStyle} 
-          name={props.badge.badge_name}
-          icon={"/assets/images/icons/"+props.badge.badge_icon}
-          description={props.badge.badge_description}
-          score={props.badge.badge_score}
+  let icon = "https://www.wappalyzer.com/images/icons/"+props.badge.badge_icon;
+
+  return <Badge 
+            show={showStyle} 
+            name={props.badge.badge_name}
+            icon={icon}
+            description={props.badge.badge_description}
+            score={props.badge.badge_score}
         />;
 }
 
